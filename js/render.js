c367d3644a2d2f22e6184370d40f3ccfc6d9cc70
bradleyturner <brad@skovistudios.com>
2016-03-03 10:48:17 -0500

    Add Bucket options

--- js/render.js
+++ js/render.js
@@ -1,21 +1,21 @@
-var obj = jQuery.parseJSON( '{ "Buckets" : [ { "Title" : "School", "Color" : "green", "Icon" : "account_balance"},{ "Title" : "Family", "Color" : "pink", "Icon" : "grade"},{ "Title" : "Family", "Color" : "red", "Icon" : "grade"},{ "Title" : "Clothes", "Color" : "orange", "Icon" : "favorite"}]}' );
-
-$.getJSON(chrome.extension.getURL('/testData.json'), function(data) {
-  console.log(data);
-
-  var items = data.items.map(function (item) {
-    return item.key + ': ' + item.value;
-  });
-  showData.empty();
-
-if (items.length) {
-  var content = '<li>' + items.join('</li><li>') + '</li>';
-  var list = $('<ul />').html(content);
-  showData.append(list);
-}
-});
-
 $(document).ready(function(){
+
+  var obj = jQuery.parseJSON( '{ "Buckets" : [ { "Title" : "School", "Color" : "green", "Icon" : "account_balance"},{ "Title" : "Family", "Color" : "blue", "Icon" : "grade"},{ "Title" : "Family", "Color" : "red", "Icon" : "grade"},{ "Title" : "Clothes", "Color" : "orange", "Icon" : "favorite"}]}' );
+
+  $.getJSON(chrome.extension.getURL('/testData.json'), function(data) {
+    console.log(data);
+
+    var items = data.items.map(function (item) {
+      return item.key + ': ' + item.value;
+    });
+    showData.empty();
+
+  if (items.length) {
+    var content = '<li>' + items.join('</li><li>') + '</li>';
+    var list = $('<ul />').html(content);
+    showData.append(list);
+  }
+  });
 
   var collection = document.createElement("ul");
   $(collection).addClass("collection")
@@ -28,11 +28,12 @@ $(document).ready(function(){
   $("#bucket-list").append(collection);
 
   for (i = 0; i < 6; i++) {
-  var item = document.createElement("div");
-  $(item).addClass("wishlist-item");
-  $(item).append('<i class="material-icons">more_vert</i><i class="material-icons">check_box_outline_blank</i><div class="card-panel white product-card"><img src="4352000_sa.jpg" class="product-image" alt="Product Image"><div class="product-price">$341.05</div></div><div class="product-info"><div class="product-title">Asus 15.6" Laptop - Intel Core i3 - 4GB Memory - 1TB Hard Drive Black X555LA-HI31103J - Best Buy</div><div class="product-description">Really want to get this before school starts!</div></div>')
-  $("#product-column").append(item);
-}
+    var item = document.createElement("div");
+    $(item).addClass("wishlist-item");
+    $(item).addClass("valign-wrapper");
+    $(item).append('<i class="material-icons valign">more_vert</i><i class="material-icons valign">check_box_outline_blank</i><div class="card-panel white product-card valign"><img src="4352000_sa.jpg" class="product-image" alt="Product Image"><div class="product-price">$341.05</div></div><div class="product-info valign"><div class="product-title">Asus 15.6" Laptop - Intel Core i3 - 4GB Memory - 1TB Hard Drive Black X555LA-HI31103J - Best Buy</div><div class="product-description">Really want to get this before school starts!</div></div>');
+    $("#product-column").append(item);
+  }
   $("#close-product").click(function(){
     $("#new-product").slideUp();
   });
@@ -40,6 +41,25 @@ $(document).ready(function(){
   $("#add-product").click(function(){
     $("#new-product").slideDown();
   });
+
+  $("#close-bucket").click(function(){
+    $("#new-bucket").slideUp();
+  });
+
+  $("#add-bucket").click(function(){
+    $("#new-bucket").slideDown();
+  });
+
+  $(".bucket").click(function(){
+    var circle = $(this).find("i");
+    var classes = (circle.attr("class"));
+    classes = classes.replace("material-icons circle","");
+    console.log(classes);
+    $(this).addClass(classes);
+    $(circle).removeClass(classes);
+    // call render
+  });
+
 
   //$(".bucket").click(function(){
   //    if (this.)
