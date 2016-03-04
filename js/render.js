263addcf9cb324f2df65a80dd8c84fe832abbf06
bradleyturner <brad@skovistudios.com>
2016-03-04 00:55:34 -0500

    Bucket menu updates
    Clicking on one bucket unselects the rest of them

--- js/render.js
+++ js/render.js
@@ -26,8 +26,47 @@ $(document).ready(function(){
   }
   $("#bucket-list").append(collection);
 
-  //$(".bucket").click(function(){
-  //    if (this.)
-  //    $(this).addClass("selected");
-  //    });
+  for (i = 0; i < 6; i++) {
+    var item = document.createElement("div");
+    $(item).addClass("wishlist-item");
+    $(item).addClass("valign-wrapper");
+    $(item).append('<i class="material-icons valign">more_vert</i><i class="material-icons valign">check_box_outline_blank</i><div class="card-panel white product-card valign"><img src="4352000_sa.jpg" class="product-image" alt="Product Image"><div class="product-price">$341.05</div></div><div class="product-info valign"><div class="product-title">Asus 15.6" Laptop - Intel Core i3 - 4GB Memory - 1TB Hard Drive Black X555LA-HI31103J - Best Buy</div><div class="product-description">Really want to get this before school starts!</div></div>');
+    $("#product-column").append(item);
+  }
+  $("#close-product").click(function(){
+    $("#new-product").slideUp();
+  });
+
+  $("#add-product").click(function(){
+    $("#new-product").slideDown();
+  });
+
+  $("#close-bucket").click(function(){
+    $("#new-bucket").slideUp();
+  });
+
+  $("#add-bucket").click(function(){
+    $("#new-bucket").slideDown();
+  });
+
+  $(".bucket").click(function(){
+    var circle = $(this).find("i");
+    var classes = (circle.attr("class"));
+    classes = classes.replace("material-icons circle","");
+    $(this).addClass(classes);
+    $(this).addClass("selected");
+    $(circle).removeClass(classes);
+    var siblings = $(this).siblings();
+    $(siblings).each(function( index ) {
+      if ($(siblings[index]).hasClass("selected")) {
+        $(siblings[index]).removeClass("selected");
+        classes = ($(siblings[index]).attr("class"));
+        classes = classes.replace("collection-item bucket","");
+        $(siblings[index]).find("i").addClass(classes);
+        $(siblings[index]).removeClass(classes);
+        console.log(classes);
+      }
+    });
+    // call render
+  });
 });
