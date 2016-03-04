5d60537787e8b69f35f58c88f9f386de3a04f33d
Duncan Fox <dbfox@mtu.edu>
2016-03-04 12:35:42 -0500

    Image selector buttons working
    Script still only works for Amazon products however

---
+++ js/popup.js
@@ -0,0 +1,36 @@
+
+var index = 0;
+var pictures;
+
+document.addEventListener('DOMContentLoaded', function() {
+                          
+    chrome.tabs.executeScript({file:"js/imagescript.js"});
+    
+    document.getElementById("left_but").addEventListener('click', function() {
+  
+        if(index == 0) {
+            index = pictures.length - 1;
+        } else {
+            index = index - 1;
+        }
+  
+        document.getElementById('prod_img').src = pictures[index];
+    });
+
+    document.getElementById("right_but").addEventListener('click', function() {
+        index = (index + 1) % pictures.length;
+        document.getElementById('prod_img').src = pictures[index];
+    });
+});
+
+chrome.runtime.onMessage.addListener(function(message) {
+    if(message.method == "gotImages") {
+                                     
+        pictures = message.images;
+                                     
+        if(pictures[0] != null) {
+                                     
+            document.getElementById('prod_img').src = pictures[0];
+        }
+    }
+});
