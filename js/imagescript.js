4c2b0e09bd8b2c766d7267b55fdf34eba8989777
Duncan Fox <dbfox@mtu.edu>
2016-03-21 13:20:18 -0400

    Price on Cards "working"

--- js/imagescript.js
+++ js/imagescript.js
@@ -1,17 +1,21 @@
 
-var images = [];
+var pictures = [];
 var area = document.getElementById('leftCol');
 var title = document.getElementById('productTitle').innerHTML;
-var price =  document.getElementsByClassName('a-color-price offer-price')[0].innerHTML;
+var price =  document.getElementsByClassName('a-color-price offer-price')[0];
+
+if(price == null) {
+    price = document.getElementsByClassName('price3P')[0];
+}
 
 findImages(area);
 
-chrome.runtime.sendMessage({method:"gotImages",images:images, message:title, cost:price});
+chrome.runtime.sendMessage({method:"gotImages",images:pictures, message:title, cost:price.innerHTML});
 
 function findImages(elem){
     if(elem.children.length == 0) {
         if(elem.nodeName.toLowerCase() === 'img' && elem.width > 70 && elem.height > 70) {
-            images.push(elem.src);
+            pictures.push(elem.src);
         }
     }
     
