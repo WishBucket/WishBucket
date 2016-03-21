098ad057c739390bc4dbdc790c864ccc06a22cc6
MichaelLGlazier <mglazier@mtu.edu>
2016-03-21 13:48:14 -0400

    Updated Storage. Should be fully capable of saving between sessions and appending/removing previously saved data.

--- js/storage.js
+++ js/storage.js
@@ -45,7 +45,7 @@
 		this.name = name;
 		this.color = color;
 		this.icon = icon;
-		this.products = new Array;
+		this.products = new Array();
 		
 		//Bucket Behavior
 		this.getName = function(){
@@ -88,41 +88,45 @@
 			return this.products.find(product);
 		};
 		this.removeProduct = function(product){
-			
+			var index = this.products.indexOf(product)
+			if(index == -1){
+				return this.products.splice(index, 1);
+			}
+			else{
+				console.log("Can't remove product; not found in array");	
+			}
 		};
-		
-		
-		
+		this.addProductList = function(productList){
+			this.products = productList;
+		};
+		this.clearProducts = function(){
+			this.products = {};
+		};
 	}
 
 //Global variable initialization
    var s = chrome.storage.sync;
-   
-   var bucketNames = [] //bucketNames act as keys to buckets Objects
 	//array of Buckets
 	var arrBucket = [];
 	
-	
 	//test code
-	//addBucket("tom", "red", "bat");
-	//addBucket("Jerry", "Blue", "cat");
-	var testList = arrBucket;
+	addBucket("tom", "red", "bat");
+	addBucket("Jerry", "Blue", "cat");
 	
-	
+	arrBucket[0].addProduct("url", 280.57, 2, "couch", "its alright");
+	arrBucket[1].addProduct("url", 10.27, 1, "Steak", "Online meat seams like a good idea");
+	arrBucket[1].addProduct("url", 12.92, 5, "Camera", "Super cheap");
+	saveChanges();
    //load list of existing buckets.
+
+	//clearMem();
    s.get('BucketList', function(data){
 		arrBucket = data.BucketList;
-		var n = arrBucket.length;
+
+		bucketify();
 		console.log(arrBucket);
 	});
 	
-	//more test code
-	//console.log(arrBucket);
-	//arrBucket[0].addProduct("url", 280.57, 2, "couch", "its alright");
-	//var testProduct = arrBucket[0].getProductList();
-	//console.log(testProduct);
-	//console.log(arrBucket[0].getProduct(testProduct[0]));
-	//s.set({'BucketList': arrBucket}, function(){})	
    
    //****************************************************************
 	
@@ -134,7 +138,7 @@
 	function addBucket(bucketName, color, icon){
 		
 		//declares Bucket object
-		var bucket = new Bucket(bucketName, color, icon)	
+		var bucket = new Bucket(bucketName, color, icon);	
 		arrBucket.push(bucket);
 
 	}
@@ -145,5 +149,43 @@
 		return arrBucket;
 	}
 	
+	function saveChanges(){
+		s.set({'BucketList': arrBucket}, function(){});
+	}
+	
+	function bucketify(){
+		try{
+			if(arrBucket != null){
+				var arrayLength = arrBucket.length;
+				for(var i = 0; i < arrayLength; i++){
+					var temp = new Bucket(arrBucket[i].name, arrBucket[i].color, arrBucket[i].icon);
+					temp.addProductList(arrBucket.products);
+					arrBucket.splice(i, 1, temp);
+				}
+			}
+			
+		}
+		catch(err){
+			console.log(err + " :error in bucketify(). Check that arrBucket is computable");
+		}
+		
+	}
+	
+	function clearMem(){
+		s.clear();	
+	}
+	
+	function removeBucket(bucket){
+			try{
+				if(arrBucket.length == 0){
+					console.log("No buckets in the array to remove");	
+				}
+				var index = arrBucket.indexOf(bucket);
+				arrBucket.splice(index, 1);
+			}
+			catch(err){
+				console.log(err + " :arrBucket object is null or non computable");	
+			}	
+	}
 
 	
